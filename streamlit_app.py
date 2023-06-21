c766818bab07d88802d9b69a46c6b07fc4693348
childersbenjamin <137271426+childersbenjamin@users.noreply.github.com>
2023-06-21 09:25:40 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,28 @@
+import openai 
+import streamlit as st
+
+# pip install streamlit-chat  
+from streamlit_chat import message
+openai.api_key = st.secrets["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"]
+def generate_response(prompt):
+    completions = openai.Completion.create(
+        engine = "text-davinci-003",
+        prompt = prompt,
+        max_tokens = 1024,
+        n = 1,
+        stop = None,
+        temperature=0.5,
+    )
+    message = completions.choices[0].text
+    return message 
+  #Creating the chatbot interface
+st.title("Musebot")
+
+# Storing the chat
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
+
 
