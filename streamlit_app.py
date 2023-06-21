5a23e9a9e0f6ab435042485bfb878a1907391ef2
childersbenjamin <137271426+childersbenjamin@users.noreply.github.com>
2023-06-21 09:31:48 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ if user_input:
     st.session_state.generated.append(output)
     if st.session_state['generated']:
     
-    for i in range(len(st.session_state['generated'])-1, -1, -1):
-        message(st.session_state["generated"][i], key=str(i))
-        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+        for i in range(len(st.session_state['generated'])-1, -1, -1):
+            message(st.session_state["generated"][i], key=str(i))
+            message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
 
